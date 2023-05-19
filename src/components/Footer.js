9d8a77f5a88aed503b51b0081c82f8897889c9fb
mp2003 <m.p6092003@gmail.com>
2023-05-19 20:12:42 +0530

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    </Stack>
+  </Box>
+);
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import { animateScroll as scroll, Link as ScrollLink } from 'react-scroll';
+
+import Logo from '../assets/images/Logo.png';
+
+const Navbar = () => {
+  const scrollToExercises = () => {
+    scroll.scrollTo(1800, {
+      smooth: true,
+      duration: 500,
+    });
+  };
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: '123px', xs: '40px' },
+        mt: { sm: '32px', xs: '20px' },
+        justifyContent: 'none',
+      }}
+      px="20px"
+    >
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+      </Link>
+      <Stack direction="row" gap="40px" fontFamily="Alegreya" fontSize="24px" alignItems="flex-end">
+        <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>
+          Home
+        </Link>
+        <ScrollLink
+          to="exercises"
+          smooth={true}
+          duration={500}
+          style={{ textDecoration: 'none', color: '#3A1212', cursor: 'pointer' }}
+          onClick={scrollToExercises}
+        >
+          Exercises
+        </ScrollLink>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Navbar;
