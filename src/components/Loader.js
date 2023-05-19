b5134e79e7b8f2f4f9a2ff969ee64be04cfa442d
mp2003 <m.p6092003@gmail.com>
2023-05-19 21:27:30 +0530

    commit

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import { InfinitySpin } from 'react-loader-spinner';
+import React from "react";
+import { Stack } from "@mui/material";
+import { InfinitySpin } from "react-loader-spinner";
 
 const Loader = () => (
-  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+  <Stack
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    width="100%"
+  >
     <InfinitySpin color="grey" />
   </Stack>
 );
